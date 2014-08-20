d288c0841cf98a33b72fe87a991096f06ce94917
jdettlaff <j.dettlaff@fu-berlin.de>
2014-08-20 13:32:41 +0200

    Implemented Tracking Algorithm Draft (SimpleTracker). Works well.

--- tracking/algorithm/simpletracker/TrackedFish.cpp
+++ tracking/algorithm/simpletracker/TrackedFish.cpp
@@ -10,3 +10,20 @@ TrackedFish::~TrackedFish(void)
 {
 }
 
+void TrackedFish::setNextPosition(cv::Point2f position) {
+	_age_of_last_known_position = 1;
+	_last_known_position = position;
+}
+
+void TrackedFish::setNextPositionUnknown() {
+	++_age_of_last_known_position;
+}
+
+cv::Point2f TrackedFish::last_known_position() const {
+	return _last_known_position;
+}
+
+unsigned TrackedFish::age_of_last_known_position() const {
+	return _age_of_last_known_position;
+}
+
