c33a496f030096219b86059eddb3474c67fbde21
Tobias von Falkenhausen <t.v.d@fu-berlin.de>
2014-09-13 15:13:42 +0200

    flickering issue resolved, picture now is fitted to window (initially and on double click)

--- source/video/VideoView.cpp
+++ source/video/VideoView.cpp
@@ -1,11 +1,5 @@
 /*	
-Copyright(c) 2010 Johannes Jordan <johannes.jordan@cs.fau.de>.
-
-This file may be licensed under the terms of of the GNU General Public
-License, version 3, as published by the Free Software Foundation. You can
-find it here: http://www.gnu.org/licenses/gpl.html
-
-changed by Tobias von Falkenhausen
+@author: Tobias von Falkenhausen
 */
 
 #include "VideoView.h"
@@ -35,30 +29,25 @@ void VideoView::showImage(cv::Mat img)
 
 void VideoView::fitToWindow()
 {
-	//int width = this->width();
-	//int height = this->height();
-	//float imgRatio = (float)_displayImage.cols/(float)_displayImage.rows;
-	//float windowRatio = (float)width/(float)height;
-	//if(windowRatio < imgRatio) 
-	//{
-	//	emit notifyGUI("windowHeight: " + StringHelper::iToSS(this->height()) + " videoHeight(original): " 
-	//		+ StringHelper::iToSS(_displayImage.rows) + " videoHeight(adapted): " + StringHelper::iToSS(width/imgRatio),MSGS::NOTIFICATION);
-	//	_zoomFactor = ((width/imgRatio)/_displayImage.rows);
-	//	_panY = (height - (width/imgRatio))/2;
-	//} else 
-	//{
-	//	emit notifyGUI("windowWidth: " + StringHelper::iToSS(this->width()) + " videoWidth(original): " 
-	//		+ StringHelper::iToSS(_displayImage.cols) + " videoWidth(adapted): " + StringHelper::iToSS(height*imgRatio),MSGS::NOTIFICATION);
-	//	_zoomFactor = (width/imgRatio)/_displayImage.rows;
-	//	_panX = (width - (height*imgRatio))/2;		
-	//}
-	//resizeGL(this->width(), this->height());
-	////Draw the scene
-	//updateGL();
-	//std::ostringstream ss;
-	//ss << _zoomFactor;
-	//std::string s(ss.str());
-	//emit notifyGUI("zoom factor" + s,MSGS::NOTIFICATION);
+	int width = this->width();
+	int height = this->height();
+	float imgRatio = (float)_displayImage.cols/(float)_displayImage.rows;
+	float windowRatio = (float)width/(float)height;
+	if(windowRatio < imgRatio) 
+	{
+		_zoomFactor = _displayImage.rows/(width/imgRatio);
+		_panY = -((height - (width/imgRatio))/2)*_zoomFactor;
+		_panX = 0;
+		emit notifyGUI("panY: " + StringHelper::iToSS(_panY),MSGS::NOTIFICATION);
+	} else 
+	{
+		_zoomFactor = _displayImage.cols/(height*imgRatio);
+		_panX = - ((width - (height*imgRatio))/2)*_zoomFactor;	
+		_panY = 0;
+	}
+	resizeGL(width, height);
+	//Draw the scene
+	updateGL();
 }
 
 void VideoView::paintGL()
@@ -82,29 +71,31 @@ void VideoView::paintGL()
 
 	glLoadIdentity();
 
+	int corner1[2] = {imageCopy.cols,0};
+	int corner2[2] = {0,0};
+	int corner3[2] = {0,imageCopy.rows};
+	int corner4[2] = {imageCopy.cols,imageCopy.rows};
+	_vertices.clear();
+	_vertices.append(QVector2D(corner1[0],corner1[1]));
+	_vertices.append(QVector2D(corner2[0],corner2[1]));
+	_vertices.append(QVector2D(corner3[0],corner3[1]));
+	_vertices.append(QVector2D(corner4[0],corner4[1]));
+
 	glVertexPointer(2, GL_FLOAT, 0, _vertices.constData());
 	glTexCoordPointer(2, GL_FLOAT, 0, _texCoords.constData());
 	glEnableClientState(GL_VERTEX_ARRAY);
 	glEnableClientState(GL_TEXTURE_COORD_ARRAY);
 
-	int ro[2] = {imageCopy.cols,0};
-	int lu[2] = {0,0};
-	int ru[2] = {0,imageCopy.rows};
-	int lo[2] = {imageCopy.cols,imageCopy.rows};
-	_vertices.clear();
-	_vertices.append(QVector2D(ro[0],ro[1]));
-	_vertices.append(QVector2D(lu[0],lu[1]));
-	_vertices.append(QVector2D(ru[0],ru[1]));
-	_vertices.append(QVector2D(lo[0],lo[1]));
+
 
 	// Non-mipmap way of mapping the texture (fast and clean):
-	// Allocate a texture name
+	// Allocate the texture
 	glGenTextures(1, &_texture);
-	// Select our current texture.
+	// Select the texture.
 	glBindTexture(GL_TEXTURE_2D, _texture); 
-	// When the texture area is larger then the image, upscale using no interpolation.
+	// If texture area is larger then the image, upscale using no interpolation.
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-	// When the texture area is smaller than the image, downsample using no interpolation.
+	// If texture area is smaller than the image, downsample using no interpolation.
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST); 
 	// wrap image onto the texture
 	glTexImage2D(GL_TEXTURE_2D, 0, 3, imageCopy.cols, imageCopy.rows, 0, GL_BGR, GL_UNSIGNED_BYTE, imageCopy.data);
@@ -121,41 +112,19 @@ void VideoView::initializeGL()
 {
 	// Create the surface we will use for the texture:
 	static const int coords[4][3] = { { +1, -1 }, { -1, -1 }, { -1, +1 }, { +1, +1 } };
+			
+	// OpenCV's coordinate system originates in the upper left corner.
+	// OpenGL originates in the lower left. Thus the image has to be flipped vertically		
 	for (int j = 0; j < 4; ++j) {
-		/* A note about texture coordinates:
-		OpenCV uses a nice, sane coordinate system with origin in the upper left corner.
-		Just like any other image processing tool (let's just forget the fact that math-wise
-		that is silly).
-		OpenGL, however, uses a math-inspired coordinate system with origin in the lower
-		left.
-		Right here, the texture is mapped, so the image is automatically flipped in the y-
-		direction. Better do it here, than actually flipping the image elsewhere.
-		*/
-		_texCoords.append(QVector2D(j == 0 || j == 3, j == 2 || j == 3));
-		//_vertices.append(QVector2D(coords[j][0], coords[j][1]));
+		_texCoords.append(QVector2D(j == 0 || j == 3, j == 2 || j == 3));	
 	}
-	
-
-	glEnable(GL_DEPTH_TEST);
-	glEnable(GL_CULL_FACE);
 	glEnable(GL_TEXTURE_2D);
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_CULL_FACE);	
 }
 void VideoView::resizeGL(int width, int height)
 {
-	// Make sure the image keeps its aspect ratio, regardless of widget size:
-	// (also, center it in the widget)
-	float imgRatio = (float)_displayImage.cols/(float)_displayImage.rows;
-	float windowRatio = (float)width/(float)height;
-	//if(windowRatio < imgRatio) 
-	//{
-	//	glViewport(0, (height-width/imgRatio)/2, width, width/imgRatio);
-	//} else 
-	//{
-	//	glViewport((width-height*imgRatio)/2, 0, height*imgRatio, height);
-	//}
 	glViewport(0,0,width, height);
-
-
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 
@@ -167,9 +136,6 @@ void VideoView::resizeGL(int width, int height)
 	float right = left + width;
 	float bottom = top + height;
 	glOrtho(left, right, bottom, top, 0.0, 1.0);
-	
-
-	//glOrtho(-1.0, +1.0, +1.0, -1.0, 0.0, 1.0);
 	glMatrixMode(GL_MODELVIEW);
 }
 
@@ -226,7 +192,7 @@ void VideoView::mousePressEvent( QMouseEvent * e )
 		_lastMPos[0] = e->x();
 		_lastMPos[1] = e->y();		
 	}
-		//variables required to map window coordinates to picture coordinates 
+	//init variables required to map window coordinates to picture coordinates 
 	GLint viewport[4];
     GLdouble modelview[16];
     GLdouble projection[16];
@@ -243,6 +209,10 @@ void VideoView::mousePressEvent( QMouseEvent * e )
 		const QPointF *localPos = new QPointF(p);
 		QMouseEvent *modifiedEvent = new QMouseEvent(e->type(),*localPos,e->screenPos(),e->button(),e->buttons(),e->modifiers());
 		emit pressEvent ( modifiedEvent );
+	}
+	if(e->button() == Qt::LeftButton && e->type() == QEvent::MouseButtonDblClick)
+	{
+		fitToWindow();
 	}
 }
 
