61eaeab6efbbc5eec5d8994ac1dbc496dbc070a2
Adam <tarach@inf.fu-berlin.de>
2015-03-12 15:31:10 +0100

    Load and store TrackedObject in multiple frames

--- source/tracking/algorithm/LandmarkTracker/LandmarkTracker.h
+++ source/tracking/algorithm/LandmarkTracker/LandmarkTracker.h
@@ -1,19 +1,28 @@
 #ifndef LandmarkTracker_H
 #define LandmarkTracker_H
 
-
 #include <memory>
-
+#include <string>
+#include "glwidget.h"
+#include "RectObject.h"
 #include "cv.h"
 #include "source/tracking/TrackingAlgorithm.h"
 
-#include "toolwindow.h"
-#include "glwidget.h"
+#include <QLabel>
+#include <QFormLayout>
+#include <QPushButton>
 
 class LandmarkTracker : public TrackingAlgorithm
 {
-private:
+	Q_OBJECT
 
+private:
+	
+	//boost::optional<size_t>	_frameNr;
+	//boost::optional<size_t> _rectId;
+	size_t	_frameNr;
+	size_t _rectId;
+	
 	cv::Rect _rectangle;
 
 	cv::Point _selectorRecStart;
@@ -24,29 +33,75 @@ private:
 	cv::Point _mouseStop;
 
 	//KML
+	cv::Point pointOfText();
+	cv::Point pot;
+	
+	std::string rectInfo();
+
 	void drawRectangle(cv::Mat image);
+	void setRectangle(cv::Mat image, cv::Scalar color);
+
+	void lockRect();
+	void unlockRect();
+	
 	cv::Rect box;
 	cv::Mat selectedRoi;
+	cv::Mat sampledRoi;
 
-	//ToolWindow *toolWindow;
-	//GLWidget *cube;
+	boost::optional<cv::Mat> img;
+	bool _lockRect;
+
+	QPixmap _pixmap;
+
+	QPixmap mat2QPixmap(const cv::Mat& mat);
 
 	void mouseMoveEvent		( QMouseEvent * e ) override;
 	void mousePressEvent	( QMouseEvent * e ) override;
 	void mouseReleaseEvent	( QMouseEvent * e ) override;
+	void keyPressEvent(QKeyEvent *e) override;
 
+	virtual std::set<Qt::Key> const& grabbedKeys() const override;
+
+	bool _movedRect;
+
+	bool _mouseMoved;
+	bool _cubeIsRoi;
+
+	
 	std::shared_ptr<GLWidget> _glwidget;
+	std::shared_ptr<QFrame> _roiFrame;
+
+	std::shared_ptr<RectObject>	_rectObject;
+
+	//ParamsWidget GUI Elements
+
+	QLabel *_roiInParams;
+	QLabel *_sampledRoiInParams;
+	
+	QPushButton *_roiButton;
+	QPushButton *_sampledRoiButton;
+
+	void initParamsWidget();
+	void updateParamsWidget();
+
+	void addRect2TrackedObj();
+
+	void setRect();
 
 public:
+
+	cv::Scalar COLOR_RECTANGLE = cv::Scalar(0, 0, 255);
+
 	LandmarkTracker	( Settings & settings, QWidget *parent );
 	~LandmarkTracker	( void ) {}
 
-	void track			( ulong /* frameNumber */, cv::Mat & /* frame */ ) override {}
+	void track(ulong /* frameNumber */, cv::Mat & /* frame */) override;
 	void paint			( cv::Mat& image, View const& view = OriginalView ) override;
 	void reset			( ) override {}
+	void postLoad() override;
 
-	//std::shared_ptr<QWidget> getToolsWidget() override { return std::static_pointer_cast<QWidget>(_glwidget); }
-
+	std::shared_ptr<QWidget> getToolsWidget() override { return std::static_pointer_cast<QWidget>(_glwidget); }
+	std::shared_ptr<QWidget> getParamsWidget() override { return std::static_pointer_cast<QWidget>(_roiFrame); }
 
 	//KML
 	void defineROI( cv::Mat image );
@@ -54,7 +109,12 @@ public:
 
 	cv::Mat getSelectedRoi();
 
-	void setToolPtr();
+	void samplingROI();
+
+public slots:
+	
+	void setCubeRoi();
+	void setCubeSampledRoi();
 
 };
 
