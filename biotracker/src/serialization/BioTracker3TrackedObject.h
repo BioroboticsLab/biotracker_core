96e4888c999532c0f01a66fb87180adb98679a84
anjoer <andyjoerg@gmail.com>
2016-10-25 13:18:12 +0200

    Restructur of classes

---
+++ biotracker/src/serialization/BioTracker3TrackedObject.h
@@ -0,0 +1,11 @@
+#ifndef BIOTRACKER3TRACKEDOBJECT_H
+#define BIOTRACKER3TRACKEDOBJECT_H
+
+
+class BioTracker3TrackedObject
+{
+public:
+    BioTracker3TrackedObject();
+};
+
+#endif // BIOTRACKER3TRACKEDOBJECT_H
