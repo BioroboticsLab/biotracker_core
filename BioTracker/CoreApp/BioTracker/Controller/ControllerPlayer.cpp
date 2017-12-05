dda84fab5b17f587a5661747721e07368e110d10
Hauke Jürgen Mönck <moenck@zedat.fu-berlin.de>
2017-12-05 18:47:59 +0100

    Fix: "Current FPS" now always shows 0 when paused

--- BioTracker/CoreApp/BioTracker/Controller/ControllerPlayer.cpp
+++ BioTracker/CoreApp/BioTracker/Controller/ControllerPlayer.cpp
@@ -125,6 +125,7 @@ void ControllerPlayer::connectModelToController() {
 
     QObject::connect(qobject_cast<MediaPlayer*>(m_Model), &MediaPlayer::renderCurrentImage, this, &ControllerPlayer::receiveRenderImage);
     QObject::connect(qobject_cast<MediaPlayer*>(m_Model), &MediaPlayer::trackCurrentImage, this, &ControllerPlayer::receiveImageToTracker);
+    QObject::connect(this, &ControllerPlayer::emitPauseState, qobject_cast<MediaPlayer*>(m_Model), &MediaPlayer::rcvPauseState);
 }
 
 void ControllerPlayer::receiveChangeDisplayImage(QString str) {
